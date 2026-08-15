9872810b920045ecabfcf1b5dad98d0f29cb02db
superroket169 <isagullu2010@gmail.com>
2026-08-15 12:21:22 +0300

    feat: button counter program

--- prog.s
+++ prog.s
@@ -1,20 +1,22 @@
 .section .text
     .globl _start
-
 _start:
-    addi x10, x0, 0
-    addi x5,  x0, 63
+    addi x10, x0, 0          # x10 = sayac
+    addi x5,  x0, 63         # x5 = maske (6 bit)
+    lui  x6,  0x1A000        # x6 = 0x1A000000 (buton+led bloğunun tabani)
 
-loop:
+wait_press:
+    lw   x7, 4(x6)
+    andi x7, x7, 1
+    beq  x7, x0, wait_press 
+
     addi x10, x10, 1
     and  x10, x10, x5
+    sw   x10, 0(x6)
 
-delay:
-    lui  x6, 0x100
-    # addi x6, x0, 5 # simülasyon için
+wait_release:
+    lw   x7, 4(x6)
+    andi x7, x7, 1
+    bne  x7, x0, wait_release
 
-delay_loop:
-    addi x6, x6, -1
-    bne  x6, x0, delay_loop
-
-    jal x0, loop
+    jal  x0, wait_press
